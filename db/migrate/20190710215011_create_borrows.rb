a199058f90f474d54396e0eae8462c8165114f7a
Meg Gutshall <meghan.gutshall@gmail.com>
2019-07-10 18:05:04 -0400

    Add associations to tables

--- db/migrate/20190710215011_create_borrows.rb
+++ db/migrate/20190710215011_create_borrows.rb
@@ -1,6 +1,8 @@
 class CreateBorrows < ActiveRecord::Migration[5.2]
   def change
     create_table :borrows do |t|
+      t.belongs_to :student_id
+      t.belongs_to :book_id
       t.date :due_date
       t.boolean :renewal
 
--- db/migrate/20190710215308_create_students_libraries.rb
+++ db/migrate/20190710215308_create_students_libraries.rb
@@ -1,7 +1,9 @@
 class CreateStudentsLibraries < ActiveRecord::Migration[5.2]
   def change
     create_table :students_libraries do |t|
-
+      t.belongs_to :student_id
+      t.belongs_to :library_id
+      
       t.timestamps
     end
   end
