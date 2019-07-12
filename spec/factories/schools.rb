2ac7527c7c9b0832c9898b9468e77a2461f2548d
Meg Gutshall <meghan.gutshall@gmail.com>
2019-07-12 18:56:31 -0400

    Messing around with FactoryBot

--- spec/factories/schools.rb
+++ spec/factories/schools.rb
@@ -1,3 +1,28 @@
-# FactoryBot.define do
+FactoryBot.define do
 
-# end
+  # school factory without associations
+  factory :school do
+    name { Faker::Address.unique.community }
+
+    # school factory without associations
+    factory :school_with_students do
+      transient do
+        students_count {10}
+      end
+
+      after(:create) do |school, evaluator|
+        build_list(:students, evaluator.students_count, school: school)
+      end
+
+      factory :student do
+        association :school
+        first_name { Faker::Name.unique.first_name }
+        last_name { Faker::Name.unique.last_name }
+        email { "#{first_name}_#{last_name}@#{school.name}.edu".downcase }
+      end
+
+    end
+
+  end
+
+end
