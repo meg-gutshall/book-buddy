53a96b9b1bf545db6b377f5ffbe8d2f9b2f0e056
Meg Gutshall <meghan.gutshall@gmail.com>
2019-07-13 13:48:40 -0400

    Create student factory

--- spec/factories/students.rb
+++ spec/factories/students.rb
@@ -1,14 +1,10 @@
 FactoryBot.define do
-
-#   factory :school do
-#     name { Faker::Address.unique.community }
-#   end
   
-  # factory :student do
-  #   association :school
-  #   first_name { Faker::Name.unique.first_name }
-  #   last_name { Faker::Name.unique.last_name }
-  #   email { "#{first_name}_#{last_name}@#{school.name}.edu".downcase }
-  # end
+  factory :student do
+    association :school
+    first_name { Faker::Name.unique.first_name }
+    last_name { Faker::Name.unique.last_name }
+    email { "#{first_name}_#{last_name}@#{school.name}.edu".downcase }
+  end
 
 end
