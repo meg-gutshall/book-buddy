5d06e91fe2ad914ceb67c2802cb390dd6d90bfea
Meg Gutshall <meghan.gutshall@gmail.com>
2019-07-14 14:20:31 -0400

    Change borrow due_date to type string

--- app/models/borrow.rb
+++ app/models/borrow.rb
@@ -1,15 +1,18 @@
 class Borrow < ApplicationRecord
-  # attributes: { created_at:datetime, due_date:datetime, renewed:boolean, student_id:integer, book_id:integer }
+  # attributes: { created_at:datetime, due_date:string, renewed:boolean, student_id:integer, book_id:integer }
   belongs_to :student
   belongs_to :book
 
   # Custom Reader/Writer Methods
   def due_date=(due_date)
-    Date.current + 2592000
+    @due_date = (Time.current + 2628000).strftime("%A, %B %e, %Y")
+    if renewed
+      (Time.current + 3838000).strftime("%A, %B %e, %Y")
+    end
   end
 
-  def renewed=(renewed)
-    due_date + 2 weeks
+  def due_date
+    @due_date
   end
 
 end
