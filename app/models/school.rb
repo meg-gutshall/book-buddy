5e1809bc80db53eb44d9ca8a622ae9646ebd1767
Meg Gutshall <meghan.gutshall@gmail.com>
2019-07-15 17:01:13 -0400

    Add additional associations on school model for easier method calls

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,6 +1,8 @@
 class School < ApplicationRecord
   # attributes: { name:string }
   has_many :students
+  has_many :borrows, through: :students
   has_many :libraries
   has_many :books, through: :libraries
+  has_many :holds, through: :books
 end
