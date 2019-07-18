5b9fa32c01da236e5c696f1ab8efbb5e46661fa0
Meg Gutshall <meghan.gutshall@gmail.com>
2019-07-18 16:29:59 -0400

    Adjust devise modules

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,8 +1,7 @@
 class Admin < ApplicationRecord
   # attributes: { first_name:string, last_name:string, email:string, password:string }
   
-  # Include default devise modules. Others available are: :confirmable, :lockable, :omniauthable, :timeoutable, and :trackable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :rememberable, :validatable
 
   has_many :schools
   
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,8 +1,7 @@
 class Student < ApplicationRecord
   # attributes: { first_name:string, last_name:string, email:string, password:string, school_id:integer }
   
-  # Include default devise modules. Others available are: :confirmable, :lockable, :timeoutable, and :trackable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :omniauthable
+  devise :database_authenticatable, :omniauthable, :registerable, :rememberable, :validatable
   
   belongs_to :school
   has_many :borrows
