35a131edceb421e4aa109b7c2e133687dccce796
Meg Gutshall <meghan.gutshall@gmail.com>
2019-07-21 00:13:56 -0400

    Organize routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,36 @@
 Rails.application.routes.draw do
-  # devise_for :admins, path: 'admin', controllers: { sessions: 'admins/sessions', registrations: 'admins/registrations' }
-  devise_for :students, controllers: { sessions: 'students/sessions', registrations: 'students/registrations' }
-
   root to: 'welcome#home'
   
-  resources :holds
-  resources :schools
-  resources :borrows
-  resources :books
-  resources :libraries
+  # devise_for :admins, path: 'admin', controllers: { sessions: 'admins/sessions', registrations: 'admins/registrations' }
+  devise_for :students, controllers: { sessions: 'students/sessions', registrations: 'students/registrations' }
+  
+  resources :schools do
+    resources :students
+    resources :libraries
+  end
+  
   resources :students
+  
+  resources :libraries do
+    resources :books
+  end
+
+  # Add nested routes
+  resources :books, only: [:show], concerns: [:borrowable, :holdable]
+  resources :students, only: [:show], concerns: [:borrowable, :holdable]
+
+  # Nested admin routes
+  # resources :admin do
+  #   resources :schools
+  # end
+
+  # Define concerns
+  concern :borrowable do
+    resources :borrows
+  end
+
+  concern :holdable do
+    resources :holds
+  end
 
 end
