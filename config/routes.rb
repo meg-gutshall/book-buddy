9d01bb4174dce011e7c5af7da91abc7e0c8c0b0a
Meg Gutshall <meghan.gutshall@gmail.com>
2019-07-22 15:36:45 -0400

    Adjust routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,13 @@
 Rails.application.routes.draw do
   root to: 'welcome#home'
-  
-  # devise_for :admins, path: 'admin', controllers: { sessions: 'admins/sessions', registrations: 'admins/registrations' }
-  devise_for :students, controllers: { sessions: 'students/sessions', registrations: 'students/registrations' }
-  
-  resources :schools do
-    resources :students
-    resources :libraries
-  end
-  
+
+  resources :schools
   resources :students
   
-  resources :libraries do
-    resources :books
-  end
-
-  # Add nested routes
-  resources :books, only: [:show], concerns: [:borrowable, :holdable]
-  resources :students, only: [:show], concerns: [:borrowable, :holdable]
-
-  # Nested admin routes
-  # resources :admin do
-  #   resources :schools
-  # end
+  resources :libraries
+  resources :books
+  resources :holds
+  resources :borrows
 
   # Define concerns
   concern :borrowable do
@@ -32,5 +17,20 @@ Rails.application.routes.draw do
   concern :holdable do
     resources :holds
   end
+  
+  devise_for :admins, path: 'admin', controllers: { sessions: 'admins/sessions', registrations: 'admins/registrations' }
+  
+  resources :schools do
+    resources :students
+    devise_for :students, controllers: { sessions: 'students/sessions', registrations: 'students/registrations' }
+    resources :libraries
+  end
+  
+  resources :libraries do
+    resources :books
+  end
+
+  resources :books, only: [:show], concerns: [:borrowable, :holdable]
+  resources :students, only: [:show], concerns: [:borrowable, :holdable]
 
 end
