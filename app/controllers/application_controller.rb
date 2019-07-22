5061d06e198d2c35389a8d0603ad99141f724b94
Meg Gutshall <meghan.gutshall@gmail.com>
2019-07-22 19:04:39 -0400

    Add 'admin_only' helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
-end
+  protect_from_forgery prepend: true
+  helper_method :admin_only
+
+  def admin_only
+    if !!current_student
+      redirect_to current_student, alert: "Permission denied: You do not have admin access."
+    end
+  end
