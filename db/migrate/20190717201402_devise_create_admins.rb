1d9234412ead0c7bf52b70b4d827e54823cdb0bd
Meg Gutshall <meghan.gutshall@gmail.com>
2019-07-23 11:39:02 -0400

    Run Devise model generator for Admin

--- db/migrate/20190717201402_devise_create_admins.rb
+++ db/migrate/20190717201402_devise_create_admins.rb
@@ -11,8 +11,8 @@ class DeviseCreateAdmins < ActiveRecord::Migration[5.2]
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
-      # t.string   :reset_password_token
-      # t.datetime :reset_password_sent_at
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
 
       ## Rememberable
       t.datetime :remember_created_at
@@ -39,7 +39,7 @@ class DeviseCreateAdmins < ActiveRecord::Migration[5.2]
     end
 
     add_index :admins, :email,                unique: true
-    # add_index :admins, :reset_password_token, unique: true
+    add_index :admins, :reset_password_token, unique: true
     # add_index :admins, :confirmation_token,   unique: true
     # add_index :admins, :unlock_token,         unique: true
   end
