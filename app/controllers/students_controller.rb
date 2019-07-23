03f26c00c424a736773a130fe2d99e25d8874a1a
Meg Gutshall <meghan.gutshall@gmail.com>
2019-07-23 13:20:09 -0400

    Delete unneeded StudentController code

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,4 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: [:show, :edit, :update, :destroy]
-  before_action :admin_only, only: [:new, :create, :edit, :update, :destroy]
   
   # GET /students
   def index
@@ -11,48 +9,7 @@ class StudentsController < ApplicationController
   def show
   end
 
-  # GET /students/new
-  def new
-    @student = Student.new
-  end
-
-  # GET /students/1/edit
-  def edit
-  end
-
-  # POST /students
-  def create
-    @student = Student.new(student_params)
-
-    if @student.save
-      redirect_to @student, notice: 'Student was successfully created.'
-    else
-      render :new
-    end
-  end
-
-  # PATCH/PUT /students/1
-  def update
-    if @student.update(student_params)
-      redirect_to @student, notice: 'Student was successfully updated.'
-    else
-      render :edit
-    end
-  end
-
-  # DELETE /students/1
-  def destroy
-    @student.destroy
-    
-    redirect_to students_url, notice: 'Student was successfully destroyed.'
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions
-    def set_student
-      @student = Student.find(params[:id])
-    end
-
     # Never trust parameters from the scary internet, only allow the white list through
     def student_params
       params.require(:student).permit(:first_name, :last_name, :email, :password, :school_id)
