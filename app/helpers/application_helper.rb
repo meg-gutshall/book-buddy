e121860c9d227ee2569ee5f28b165fb5c0ffca44
Meg Gutshall <meghan.gutshall@gmail.com>
2019-07-23 23:03:44 -0400

    Refactor #hide_admin_action helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,7 @@ module ApplicationHelper
   # TODO: Create index table layouts for admin and non-admin users
   
   def hide_admin_action(link_code)
-    if !!current_admin
-      link_code
-    end
+    link_code if !!current_admin
   end
   
 end
