a81468bcf002626582319321d407ed0905e2d399
Meg Gutshall <meghan.gutshall@gmail.com>
2019-08-01 18:15:05 -0400

    Add helper methods

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -1,6 +1,7 @@
 class SchoolsController < ApplicationController
   before_action :set_school, only: [:show, :edit, :update, :destroy]
-  before_action :admin_only, only: [:new, :create, :edit, :update, :destroy]
+  before_action :admin_district_stray, only: [:show, :edit, :update, :destroy]
+  before_action :admin_only
 
   # GET /schools
   def index
