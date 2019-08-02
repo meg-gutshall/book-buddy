17a9d8a4a7f9ea684caf8dc16266a82c98d7a5fe
Meg Gutshall <meghan.gutshall@gmail.com>
2019-08-02 13:08:21 -0400

    Add #admin_district_stray helper to SchoolsController

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -56,6 +56,13 @@ class SchoolsController < ApplicationController
       @school = School.find_by(id: params[:id])
     end
 
+    def admin_district_stray
+      unless current_admin.school_ids.include?(params[:id].to_i)
+        flash[:alert] = "Permission denied: You may not view or edit another school districts' content."
+        redirect_to root_path and return
+      end
+    end
+
     # Never trust parameters from the scary internet, only allow the white list through
     def school_params
       params.require(:school).permit(:name, :admin_id)
