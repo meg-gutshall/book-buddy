a9572682d5735e115ccc82739f821a3ff8444da7
Meg Gutshall <meghan.gutshall@gmail.com>
2019-08-03 11:17:42 -0400

    Remove unwanted devise utilities

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,7 +2,7 @@ class Admin < ApplicationRecord
   # attributes: { first_name:string, last_name:string, email:string, password:string }
   has_many :schools
 
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :validatable
   
   # Class Methods: full_name
 
--- db/migrate/20190717201402_devise_create_admins.rb
+++ db/migrate/20190717201402_devise_create_admins.rb
@@ -11,11 +11,11 @@ class DeviseCreateAdmins < ActiveRecord::Migration[5.2]
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
+      # t.string   :reset_password_token
+      # t.datetime :reset_password_sent_at
 
       ## Rememberable
-      t.datetime :remember_created_at
+      # t.datetime :remember_created_at
 
       ## Trackable
       # t.integer  :sign_in_count, default: 0, null: false
@@ -39,7 +39,7 @@ class DeviseCreateAdmins < ActiveRecord::Migration[5.2]
     end
 
     add_index :admins, :email,                unique: true
-    add_index :admins, :reset_password_token, unique: true
+    # add_index :admins, :reset_password_token, unique: true
     # add_index :admins, :confirmation_token,   unique: true
     # add_index :admins, :unlock_token,         unique: true
   end
