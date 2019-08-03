c8d764ad31e6940de8ab662ba3f974dda6fa6d6d
Meg Gutshall <meghan.gutshall@gmail.com>
2019-08-03 11:18:43 -0400

    Rerun migrations

--- db/schema.rb
+++ db/schema.rb
@@ -20,13 +20,9 @@ ActiveRecord::Schema.define(version: 2019_07_17_201402) do
     t.string "last_name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
   create_table "books", force: :cascade do |t|
@@ -87,13 +83,9 @@ ActiveRecord::Schema.define(version: 2019_07_17_201402) do
     t.string "last_name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_students_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_students_on_reset_password_token", unique: true
     t.index ["school_id"], name: "index_students_on_school_id"
   end
 
