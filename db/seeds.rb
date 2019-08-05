0f10dc353ef2b7098a6f830619e4c2996a7b3810
Meg Gutshall <meghan.gutshall@gmail.com>
2019-08-05 13:44:15 -0400

    Add more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,7 +21,21 @@ pip = Admin.create(
   email: "pip@pip.com"
 )
 
-# Faker school objects
+les = Admin.create(
+  first_name: "Les",
+  last_name: "Gutshall",
+  password: "password",
+  email: "les@les.com"
+)
+
+ei = Admin.create(
+  first_name: "Eileen",
+  last_name: "Gutshall",
+  password: "password",
+  email: "ei@ei.com"
+)
+
+# School objects
 otis = School.create(
   name: "Otis Elementary School"
 )
@@ -29,6 +43,24 @@ otis = School.create(
 redding = School.create(
   name: "Redding High School"
 )
+
+marv = School.create(
+  name: "Marvin Middle School"
+)
+
+gaye = School.create(
+  name: "Gaye High School"
+)
+
+meg.schools << otis
+meg.schools << redding
+pip.schools << otis
+pip.schools << redding
+
+les.schools << marv
+les.schools << gaye
+ei.schools << marv
+ei.schools << gaye
 
 # Faker student objects
 s1 = Student.create(
@@ -47,7 +79,23 @@ s2 = Student.create(
   email: "jill@email.com"
 )
 
-58.times do
+s3 = Student.create(
+  school_id: 3,
+  first_name: "Jack",
+  last_name: "Student",
+  password: "password",
+  email: "jack@email.com"
+)
+
+s4 = Student.create(
+  school_id: 4,
+  first_name: "Jen",
+  last_name: "Student",
+  password: "password",
+  email: "jen@email.com"
+)
+
+30.times do
   Student.create(
     school_id: 1,
     first_name: Faker::Name.first_name,
@@ -57,9 +105,29 @@ s2 = Student.create(
   )
 end
 
-74.times do
+45.times do
   Student.create(
     school_id: 2,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    password: Faker::Internet.password,
+    email: Faker::Internet.free_email
+  )
+end
+
+25.times do
+  Student.create(
+    school_id: 3,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    password: Faker::Internet.password,
+    email: Faker::Internet.free_email
+  )
+end
+
+30.times do
+  Student.create(
+    school_id: 4,
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
     password: Faker::Internet.password,
@@ -82,6 +150,20 @@ end
   )
 end
 
+2.times do
+  Library.create(
+    school_id: 3,
+    room: Faker::Number.number(3)
+  )
+end
+
+1.times do
+  Library.create(
+    school_id: 4,
+    room: "Paley Library"
+  )
+end
+
 # Faker book objects
 44.times do
   Book.create(
@@ -93,7 +175,7 @@ end
   )
 end
 
-83.times do
+27.times do
   Book.create(
     library_id: 2,
     title: Faker::Book.title,
@@ -103,7 +185,7 @@ end
   )
 end
 
-54.times do
+31.times do
   Book.create(
     library_id: 3,
     title: Faker::Book.title,
@@ -113,7 +195,7 @@ end
   )
 end
 
-67.times do
+19.times do
   Book.create(
     library_id: 4,
     title: Faker::Book.title,
@@ -123,9 +205,39 @@ end
   )
 end
 
-57.times do
+39.times do
   Book.create(
     library_id: 5,
+    title: Faker::Book.title,
+    author: Faker::Book.author,
+    genre: Faker::Book.genre,
+    copies: Faker::Number.between(1, 3)
+  )
+end
+
+44.times do
+  Book.create(
+    library_id: 6,
+    title: Faker::Book.title,
+    author: Faker::Book.author,
+    genre: Faker::Book.genre,
+    copies: Faker::Number.between(1, 3)
+  )
+end
+
+27.times do
+  Book.create(
+    library_id: 7,
+    title: Faker::Book.title,
+    author: Faker::Book.author,
+    genre: Faker::Book.genre,
+    copies: Faker::Number.between(1, 3)
+  )
+end
+
+57.times do
+  Book.create(
+    library_id: 8,
     title: Faker::Book.title,
     author: Faker::Book.author,
     genre: Faker::Book.genre,
